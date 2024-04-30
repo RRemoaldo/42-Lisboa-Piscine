44b99024dfd49d99df06ca93bd204564f47a7bd5
RRemoaldo <remoaldorafael@gmail.com>
2024-04-30 15:11:13 +0100

    update

--- common_core/libft/ft_substr.c
+++ common_core/libft/ft_substr.c
@@ -14,26 +14,26 @@
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-    char            *sub;
-    size_t          s_len;
-    size_t          i;
+	char	*sub;
+	size_t	s_len;
+	size_t	i;
 
-    if (!s)
-        return (NULL);
-    s_len = ft_strlen(s);
-    if (start >= s_len)
-        return (ft_strdup(""));
-    len = (start + len > s_len) ? s_len - start : len;
-    sub = malloc(sizeof(char) * (len + 1));
-    if (!sub)
-        return (NULL);
-    i = 0;
-    while (i < len && s[start])
-    {
-        sub[i] = s[start];
-        i++;
-        start++;
-    }
-    sub[i] = '\0';
-    return (sub);
+	if (!s)
+		return (NULL);
+	s_len = ft_strlen(s);
+	if (start >= s_len)
+		return (ft_strdup(""));
+	len = (start + len > s_len) ? s_len - start : len;
+	sub = malloc(sizeof(char) * (len + 1));
+	if (!sub)
+		return (NULL);
+	i = 0;
+	while (i < len && s[start])
+	{
+		sub[i] = s[start];
+		i++;
+		start++;
+	}
+	sub[i] = '\0';
+	return (sub);
 }
